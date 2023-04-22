6310834142261f2c7e7e125c2def26467a64341d
Burak Ergen <burak.ergen@cooksoft.com.tr>
2023-04-22 21:41:18 +0300

    learn scanf

--- learn-c/main.c
+++ learn-c/main.c
@@ -12,16 +12,41 @@ int main()
 
 	 */
 
-   int number1= 2;
-	float number2= 2.5;
-	double number3= 4.5;
-   char ca= 'a';
+//    int number1= 2;
+// 	float number2= 2.5;
+// 	double number3= 4.5;
+//    char ca= 'a';
 
-	printf("%d\n%.2f\n%.2lf\n%c",number1,number2,number3,ca);
+// 	printf("%d\n%.2f\n%.2lf\n%c",number1,number2,number3,ca);
 
-	printf("\nHello C !");
+// 	printf("\nHello C !");
 	//return 0; -- not necessarily
 
 
+//	int number;
+//
+//	printf("Please enter a number\n");
+//	scanf("%d",&number);
+//	printf("Your number %d",number);
+//
+
+//	float number;
+//
+//	printf("Please enter a number\n");
+//	scanf("%f",&number);
+//	printf("Your number %.2f",number);
+
+//   double number;
+//
+//	printf("Please enter a number\n");
+//	scanf("%lf",&number);
+//	printf("Your number %.2lf",number);
+
+
+   char character;
+
+	printf("Please enter a number\n");
+	scanf("%c",&character);
+	printf("Your character %c",character);
 
 }
